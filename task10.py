43ec2795591e8753e2b0d76fca1a48e4e1d93351
Liliana <91615473+NaniiiGock@users.noreply.github.com>
2022-11-13 19:11:59 +0200

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,1 +1,13 @@
 
+from math import inf
+class Solution:
+    def minCut(self,s: str) -> int:
+        result = [i for i in range(-1,len(s))]
+        for start in range(len(s)):
+            for finish in range(start, len(s)):
+                if s[start:finish] == s[finish:start:-1]:
+                    #if part is palindrome
+                    if result[finish+1] > result[start] +1:
+                        result[finish+1] = result[start]+1
+                        #change to lower number of slices
+        return result[-1]
