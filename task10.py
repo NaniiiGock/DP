c1905ddba4870724bbf75a1273367f3c84c548f8
Liliana <91615473+NaniiiGock@users.noreply.github.com>
2022-11-14 09:28:08 +0200

    Update task10.py

--- task10.py
+++ task10.py
@@ -11,3 +11,18 @@ class Solution:
                         result[finish+1] = result[start]+1
                         #change to lower number of slices
         return result[-1]
+
+    
+    def solve(s):
+    lenn = len(s)
+    cuts = [i for i in range(lenn)]
+    matrix = [[False for j in range(lenn)] for j in range(lenn)] 
+    for i in range(0,lenn):
+        for j in range(0, i+1):
+            if s[i] == s[j] and (i <= j+1 or matrix[j+1][i-1]):
+                matrix[j][i] = True
+                if j != 0:
+                    cuts[i] = min(cuts[i], cuts[j-1]+1)
+                else:
+                    cuts[i] = 0
+    return cuts[-1]
